8a01e7fca865d8eb730dab56c418278eb424134c
Leen Alsulami <90353695+LeenSulami@users.noreply.github.com>
2023-08-06 09:23:48 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,3 @@
-// $(document).ready(function () {
-//   $(".navbar-toggler").click(function () {
-//     $(".navbar-collapse").slideToggle();
-//   });
-// });
-// const sidebarToggle = document.getElementById("sidebarToggle");
-// const sidebar = document.getElementById("sidebar");
-
-// sidebarToggle.addEventListener("click", function () {
-//   sidebar.classList.toggle("active");
-//   sidebarToggle.classList.toggle("active");
-// });
-
 $(document).ready(function () {
   $(window).scroll(function () {
     $(".content-based-scroll").each(function () {
